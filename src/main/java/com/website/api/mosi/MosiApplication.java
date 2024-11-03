0cf45f9f53fb0dcfbab7c29aa44e67bf5285863a
fakhari <fmohsen22@gmail.com>
2024-11-04 00:15:40 +0100

    update

---
+++ src/main/java/com/website/api/mosi/MosiApplication.java
@@ -0,0 +1,13 @@
+package com.website.api.mosi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/website/api/mosi/TimeController.java
@@ -0,0 +1,26 @@
+package com.website.api.mosi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class TimeController {
+
+    @GetMapping("/api/time")
+    public Map<String, String> getCurrentTimes() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        Map<String, String> times = new HashMap<>();
+        times.put("tehran", LocalDateTime.now(ZoneId.of("Asia/Tehran")).format(formatter));
+        times.put("vienna", LocalDateTime.now(ZoneId.of("Europe/Vienna")).format(formatter));
+        times.put("sydney", LocalDateTime.now(ZoneId.of("Australia/Sydney")).format(formatter));
+
+        return times;
+    }
+}
