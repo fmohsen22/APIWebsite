054af3b0b6e66be828a8d2e572c3cf25b2e20846
fakhari <fmohsen22@gmail.com>
2024-11-10 21:28:15 +0100

    cahtbot

--- src/main/java/com/website/api/mosi/ChatController.java
+++ src/main/java/com/website/api/mosi/ChatController.java
@@ -3,6 +3,9 @@ package com.website.api.mosi;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/chat")
 public class ChatController {
@@ -11,8 +14,13 @@ public class ChatController {
     public ResponseEntity<String> receiveMessage(@RequestBody ChatMessage message) {
         // For now, echo the received message back
 
-        String responseText = "You said: " + message.getText();
-        return ResponseEntity.ok(responseText);
+        String responseText =  message.getText();
+
+        // Create a map to hold the response JSON structure
+        Map<String, String> response = new HashMap<>();
+        response.put("reply", responseText);
+
+        return ResponseEntity.ok(String.valueOf(response));
     }
 }
 
