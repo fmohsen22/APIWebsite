2fb4e233a8686aea040a8556f277ab1c2f247f4c
mohsenfakhari <fmohsen22@gmail.com>
2024-12-21 00:15:47 +0100

    chatbot

--- src/main/java/com/website/api/mosi/ChatController.java
+++ src/main/java/com/website/api/mosi/ChatController.java
@@ -1,5 +1,7 @@
 package com.website.api.mosi;
 
+import com.website.api.mosi.api.rest.GenericHttpClient;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,8 @@ import java.util.Map;
 @RequestMapping("/api/chat")
 public class ChatController {
 
+    private GenericHttpClient httpClient;
+
     @PostMapping("/message")
     public ResponseEntity<Map<String, String>> receiveMessage(@RequestBody ChatMessage message) {
         // Get the message text sent by the user
@@ -18,14 +22,21 @@ public class ChatController {
 
         // Prepare a response based on the user's message
         String responseText = null;
-        if (userText.toLowerCase().contains("hello")) {
-            responseText = "Hello! How can I assist you today? 😊";
-        } else if (userText.toLowerCase().contains("price")) {
-            responseText = "Our service prices vary. Could you specify the service you're interested in?";
-        } else if (userText.toLowerCase().contains("appointment")) {
-            responseText = "You can book an appointment through our website's 'Time Booking' section!";
-        }
+//        if (userText.toLowerCase().contains("hello")) {
+//            responseText = "Hello! How can I assist you today? 😊";
+//        } else if (userText.toLowerCase().contains("price")) {
+//            responseText = "Our service prices vary. Could you specify the service you're interested in?";
+//        } else if (userText.toLowerCase().contains("appointment")) {
+//            responseText = "You can book an appointment through our website's 'Time Booking' section!";
+//        }
 
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+
+        responseText = String.valueOf(httpClient.sendRequest("https://fmohsen22-chatbot-cosmeticshop.hf.space/chat",
+                HttpMethod.POST, headers, "{\n" +
+                        "  \"user_input\": \""+userText+"\"\n" +
+                        "}"));
 
 
         // Create a map to hold the response JSON structure
